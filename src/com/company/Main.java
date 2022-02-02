611362bd72448eb3cfd510ae7e3e614bbdd229b8
chamara <2019t00375@stu.cmb.ac.lk>
2022-02-03 00:52:28 +0530

    I add car park class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,40 @@ package com.company;
 
 public class Main {
 
+        public class CarOwner { //car park class
+        String ownerid;
+        String mobile;
+        String vehiclenumber;
+        String detailsid;
+        String exitid;
+
+        HashMap<String, ArrayList<String>> carowner = new HashMap<>();
+        HashMap<String, String> allowners = new HashMap<>();
+
+
+        public void enter(){ //enter owner id
+
+            allowners.put(ownerid,vehiclenumber);
+            carowner.put(ownerid, new ArrayList<>());
+            carowner.get(ownerid).add("owner id : "+ownerid);
+            carowner.get(ownerid).add("mobile number : "+mobile);
+            carowner.get(ownerid).add("vehicle number : "+vehiclenumber);
+        }
+        public void details(){ // view all car owner details
+
+
+            for (String i : carowner.keySet()) {
+                System.out.println( i + " ---> " + carowner.get(i));
+            }
+
+        }
+
+        }
+
+
+
     public static void main(String[] args) {
-	// write your code here
+
     }
+
 }
